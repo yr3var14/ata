0bca8ea2f7895d5d19dd7724a2b30433a6dc76d3
Sonali Saxena <sonali.saxena.cse12@iitbhu.ac.in>
2015-02-19 23:19:59 +0530

    Update code.py

--- src/code.py
+++ src/code.py
@@ -1,14 +1,30 @@
-#code to compare different classifiers on the basis of their prediction accuracy , time taken to train and test , f1-score and dimensionality
-#on the basis of which we can decide which classifier is suitable for our model
-from __future__ import print_function
 
+======================================================
+Classification of text documents using sparse features
+======================================================
+
+code to compare different classifiers on the basis of their prediction accuracy , time taken to train and test , f1-score and dimensionality
+on the basis of which we can decide which classifier is suitable for our model
+
+"""
+
+# Author: Peter Prettenhofer <peter.prettenhofer@gmail.com>
+#         Olivier Grisel <olivier.grisel@ensta.org>
+#         Mathieu Blondel <mathieu@mblondel.org>
+#         Lars Buitinck <L.J.Buitinck@uva.nl>
+# License: BSD 3 clause
+
+from __future__ import print_function
+import sklearn
+from sklearn import *
 import logging
 import numpy as np
 from optparse import OptionParser
 import sys
 from time import time
-import sklearn
+import matplotlib.pyplot as plt
 
+from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.feature_selection import SelectKBest, chi2
@@ -23,9 +39,27 @@ from sklearn.neighbors import NearestCentroid
 from sklearn.utils.extmath import density
 from sklearn import metrics
 
+
+# Display progress logs on stdout
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+
 # parse commandline arguments
 op = OptionParser()
-
+op.add_option("--report",
+              action="store_true", dest="print_report",
+              help="Print a detailed classification report.")
+op.add_option("--chi2_select",
+              action="store", type="int", dest="select_chi2",
+              help="Select some number of features using a chi-squared test")
+op.add_option("--confusion_matrix",
+              action="store_true", dest="print_cm",
+              help="Print the confusion matrix.")
+op.add_option("--top10",
+              action="store_true", dest="print_top10",
+              help="Print ten most discriminative terms per class"
+                   " for every classifier.")
 op.add_option("--all_categories",
               action="store_true", dest="all_categories",
               help="Whether to use all categories or not.")
@@ -35,6 +69,10 @@ op.add_option("--use_hashing",
 op.add_option("--n_features",
               action="store", type=int, default=2 ** 16,
               help="n_features when using the hashing vectorizer.")
+op.add_option("--filtered",
+              action="store_true",
+              help="Remove newsgroup information that is easily overfit: "
+                   "headers, signatures, and quoting.")
 
 (opts, args) = op.parse_args()
 if len(args) > 0:
@@ -58,17 +96,22 @@ else:
         'sci.space',
     ]
 
-print("Loading dataset for categories:")
+if opts.filtered:
+    remove = ('headers', 'footers', 'quotes')
+else:
+    remove = ()
+
+print("Loading 20 newsgroups dataset for categories:")
 print(categories if categories else "all")
 
-data_train = sklearn.datasets.load_files('/home/rg/Desktop/train', description=None, categories=None, load_content=True, shuffle=True, encoding='latin1', charset=None, charset_error=None, decode_error='strict', random_state=42)
+data_train = sklearn.datasets.load_files('/home/sonali/Desktop/20news-bydate-train', description=None, categories=None, load_content=True, shuffle=True, encoding='latin1', charset=None, charset_error=None, decode_error='strict', random_state=42)
 
 
-data_test = sklearn.datasets.load_files('/home/rg/Desktop/test', description=None, categories=None, load_content=True, shuffle=True, encoding='latin1', charset=None, charset_error=None, decode_error='strict', random_state=42)
+data_test = sklearn.datasets.load_files('/home/sonali/Desktop/20news-bydate-train', description=None, categories=None, load_content=True, shuffle=True, encoding='latin1', charset=None, charset_error=None, decode_error='strict', random_state=42)
 
 print('data loaded')
 
-categories = data_train.target_names   
+categories = data_train.target_names    # for case categories == None
 
 
 def size_mb(docs):
@@ -110,6 +153,22 @@ print("done in %fs at %0.3fMB/s" % (duration, data_test_size_mb / duration))
 print("n_samples: %d, n_features: %d" % X_test.shape)
 print()
 
+if opts.select_chi2:
+    print("Extracting %d best features by a chi-squared test" %
+          opts.select_chi2)
+    t0 = time()
+    ch2 = SelectKBest(chi2, k=opts.select_chi2)
+    X_train = ch2.fit_transform(X_train, y_train)
+    X_test = ch2.transform(X_test)
+    print("done in %fs" % (time() - t0))
+    print()
+
+
+def trim(s):
+    """Trim string to fit on terminal (assuming 80-column display)"""
+    return s if len(s) <= 80 else s[:77] + "..."
+
+
 # mapping from integer feature name to original token string
 if opts.use_hashing:
     feature_names = None
@@ -139,8 +198,23 @@ def benchmark(clf):
     if hasattr(clf, 'coef_'):
         print("dimensionality: %d" % clf.coef_.shape[1])
         print("density: %f" % density(clf.coef_))
+
+        if opts.print_top10 and feature_names is not None:
+            print("top 10 keywords per class:")
+            for i, category in enumerate(categories):
+                top10 = np.argsort(clf.coef_[i])[-10:]
+                print(trim("%s: %s"
+                      % (category, " ".join(feature_names[top10]))))
         print()
 
+    if opts.print_report:
+        print("classification report:")
+        print(metrics.classification_report(y_test, pred,
+                                            target_names=categories))
+
+    if opts.print_cm:
+        print("confusion matrix:")
+        print(metrics.confusion_matrix(y_test, pred))
 
     print()
     clf_descr = str(clf).split('(')[0]
@@ -151,8 +225,7 @@ results = []
 for clf, name in (
         (RidgeClassifier(tol=1e-2, solver="lsqr"), "Ridge Classifier"),
         (Perceptron(n_iter=50), "Perceptron"),
-        (PassiveAggressiveClassifier(n_iter=50), "Passive-Aggressive"),
-        (KNeighborsClassifier(n_neighbors=10), "kNN")):
+        (PassiveAggressiveClassifier(n_iter=50), "Passive-Aggressive")):
     print('=' * 80)
     print(name)
     results.append(benchmark(clf))
@@ -203,3 +276,30 @@ class L1LinearSVC(LinearSVC):
 print('=' * 80)
 print("LinearSVC with L1-based feature selection")
 results.append(benchmark(L1LinearSVC()))
+
+
+# make some plots
+
+indices = np.arange(len(results))
+
+results = [[x[i] for x in results] for i in range(4)]
+
+clf_names, score, training_time, test_time = results
+training_time = np.array(training_time) / np.max(training_time)
+test_time = np.array(test_time) / np.max(test_time)
+
+plt.figure(figsize=(12, 8))
+plt.title("Score")
+plt.barh(indices, score, .2, label="score", color='r')
+plt.barh(indices + .3, training_time, .2, label="training time", color='g')
+plt.barh(indices + .6, test_time, .2, label="test time", color='b')
+plt.yticks(())
+plt.legend(loc='best')
+plt.subplots_adjust(left=.25)
+plt.subplots_adjust(top=.95)
+plt.subplots_adjust(bottom=.05)
+
+for i, c in zip(indices, clf_names):
+    plt.text(-.3, i, c)
+
+plt.show()
