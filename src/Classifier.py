2624fadd055051cc2dd385ed03bae7bfb319f1f6
rohit gupta <rohit.gupta.cse12@iitbhu.ac.in>
2015-02-21 09:03:00 +0000

    Update Classifier.py

--- src/Classifier.py
+++ src/Classifier.py
@@ -41,3 +41,78 @@ results = Cls.learn_infer_evaluate (train, test, classifiers=Classifiers, metric
 Cls.plot(results)
 """
 
+"""
+@author:- rohitguptacse12 and sonacse12
+this is the classifier class which takes the features from the featureExtractor class
+
+"""
+
+# p = FeatureExtractor()
+# X_train , y_train = p.getFeatures('BOW-lv')
+
+# q = FeatureExtractor()
+# X_test , y_test = q.getFeatures('BOW-lv')
+#
+from __future__ import print_function
+
+import logging
+import numpy as np
+from optparse import OptionParser
+import sys
+from time import time
+import sklearn
+
+from sklearn.svm import SVC
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.utils.extmath import density
+from sklearn import metrics
+
+
+
+
+class Classifier:
+	
+    results = []
+
+    def getLearn(self, option):
+
+	if option == 'svm':
+	    results.append(evaluation(SVC(C=1)))
+
+	elif option == 'multinomial':
+	    results.append(evaluation(MultinomialNB(alpha=.01)))
+
+	elif option == 'bernouli':
+	    results.append(evaluation(BernoulliNB(alpha=.01)))
+
+    def evaluation(clf):
+    	print('_' * 80)
+    	print("Training: ")
+    	print(clf)
+    	t0 = time()
+    	clf.fit(X_train, y_train)
+    	train_time = time() - t0
+    	print("train time: %0.3fs" % train_time)
+
+    	t0 = time()
+    	pred = clf.predict(X_test)
+    	test_time = time() - t0
+    	print("test time:  %0.3fs" % test_time)
+	
+    	score = metrics.f1_score(y_test, pred)
+    	print("f1-score:   %0.3f" % score)
+	
+    	if hasattr(clf, 'coef_'):
+    	    print("dimensionality: %d" % clf.coef_.shape[1])
+    	    print("density: %f" % density(clf.coef_))
+    	    print()
+	
+	
+    	print()
+    	clf_descr = str(clf).split('(')[0]
+    	return clf_descr, score, train_time, test_time
+	
+Cls=Classifer()
+p.getLearn('svm')
+		
+		
