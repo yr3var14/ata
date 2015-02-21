d93e91f7baf7e4de1e4b6a8574931001a6d5840c
Sonali Saxena <sonali.saxena.cse12@iitbhu.ac.in>
2015-02-21 14:06:35 +0530

    Update FeatureExtractor.py

--- src/FeatureExtractor.py
+++ src/FeatureExtractor.py
@@ -26,18 +26,20 @@ from sklearn.feature_selection import chi2
 from sklearn.feature_selection import *
 from sklearn.feature_extraction.text import *
 from sklearn import svm
-from sklearn.datasets import load_iris
+import gensim
+import nltk
+from nltk.corpus import brown
 class FeatureExtractor:
 	Vocab = []
 	iris=[]
-	
+	target=[]
 	def __init__ (self,filename):
 		
 		#self.iris = load_iris()
 		#cat=['alt.atheism','soc.religion.christian','comp.graphics', 'sci.med']
 		#filename='/home/sonali/Desktop/20news-bydate-train'
 		self.Vocab= sklearn.datasets.load_files(filename, description=None, categories=None, load_content=True, shuffle=True, encoding='latin1', charset=None, charset_error=None, decode_error='strict', random_state=42)
-
+		
 	def getFeatures(self, option):
 		if option=='tfidf':
 			tf=TfidfTransformer()
@@ -51,18 +53,22 @@ class FeatureExtractor:
 			X=vectorizer.fit_transform(self.Vocab.data)
 			tf=TfidfTransformer()
 			X_new=tf.fit_transform(X)
-		elif option=='w2v':
-			word2vec()
+		elif option=='skB':
+			vectorizer = SelectKBest(chi2, k=2)
+			X_new =vectorizer.fit_transform(self.Vocab.data,self.Vocab.target )
+		
 		elif option=='BOW-lv':
 			vectorizer = CountVectorizer()
 			X=vectorizer.fit_transform(self.Vocab.data)
 			sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
 			X_new=sel.fit_transform(X)
 		else:
-			print 'Please Select a difined type of feature'
-		return X_new
-	def word2vec(self):
-		print 'have to work on it word2vec'
+			print 'Please Select a defined type of feature'
+		return X_new, self.Vocab.target
+	def word2vecmodel(self):
+		b=gensim.models.Word2Vec(brown.sents())
+                b.save('/home/sonali/Desktop/corporamodel')
+                return b  #returns a model which define vector for each word depending on contex
 #compute and return vectors
 #more option are to be added
 #def any summary print visualize function
