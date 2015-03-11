0821c00ba56d8d19486893ad53c759c72f74365d
Sonali Saxena <sonali.saxena.cse12@iitbhu.ac.in>
2015-03-11 23:27:08 +0530

    Update modelvalidation.py

--- src/modelvalidation.py
+++ src/modelvalidation.py
@@ -11,7 +11,7 @@ def validationerror(option,X_train,X_test,y_train,y_test):
 
 	for alpha in alphas:
 		if option == 'svm':
-			clf=SVC(gamma=alpha,C=1)
+			clf=SVC(C=alpha)
 
 		elif option == 'multinomial':
 			clf=MultinomialNB(alpha=alpha)
