3383871be3ffe351fc9e5410cd828c2c4a668836
Sonali Saxena <sonali.saxena.cse12@iitbhu.ac.in>
2015-03-20 01:56:08 +0530

    Update FeatureExtractor.py

--- src/FeatureExtractor.py
+++ src/FeatureExtractor.py
@@ -16,7 +16,7 @@ Example code:
 
 FE = FeatureExtractor(Text)
 f_w2v = FE.getFeatures (option='w2v', text [,options])
-
+@edited to return features on train data also
 """
 
 import sklearn
@@ -31,40 +31,47 @@ import nltk
 from nltk.corpus import brown
 class FeatureExtractor:
 	Vocab = []
+	Test_cab=[]
+	vectoriser=CountVectorizer()
 	iris=[]
 	target=[]
-	def __init__ (self,filename):
+	def __init__ (self,filetrain,filetest):
 		
 		#self.iris = load_iris()
 		#cat=['alt.atheism','soc.religion.christian','comp.graphics', 'sci.med']
 		#filename='/home/sonali/Desktop/20news-bydate-train'
-		self.Vocab= sklearn.datasets.load_files(filename, description=None, categories=None, load_content=True, shuffle=True, encoding='latin1', charset=None, charset_error=None, decode_error='strict', random_state=42)
+		self.Vocab= sklearn.datasets.load_files(filetrain, description=None, categories=None, load_content=True, shuffle=True, encoding='latin1', charset=None, charset_error=None, decode_error='strict', random_state=42)
+		self.Test_cab= sklearn.datasets.load_files(filetest, description=None, categories=None, load_content=True, shuffle=True, encoding='latin1', charset=None, charset_error=None, decode_error='strict', random_state=42)
 		
 	def getFeatures(self, option):
 		if option=='tfidf':
-			tf=TfidfTransformer()
-			X_new=tf.fit_transform(self.Vocab.data)
+			self.vectorizer=TfidfTransformer()
+			X_new=self.vectorizer.fit_transform(self.Vocab.data)
 			
 		elif option=='bow':
-			vectorizer = CountVectorizer()
-			X_new=vectorizer.fit_transform(self.Vocab.data)
+			self.vectorizer = CountVectorizer()
+			X_new=self.vectorizer.fit_transform(self.Vocab.data)
 		elif option=='svm':
 			vectorizer = CountVectorizer()
 			X=vectorizer.fit_transform(self.Vocab.data)
-			tf=TfidfTransformer()
-			X_new=tf.fit_transform(X)
+			self.vectorizer=TfidfTransformer()
+			X_new=self.vectorizer.fit_transform(X)
 		elif option=='skB':
-			vectorizer = SelectKBest(chi2, k=2)
-			X_new =vectorizer.fit_transform(self.Vocab.data,self.Vocab.target )
+			self.vectorizer = SelectKBest(chi2, k=2)
+			X_new =self.vectorizer.fit_transform(self.Vocab.data,self.Vocab.target )
 		
 		elif option=='BOW-lv':
 			vectorizer = CountVectorizer()
 			X=vectorizer.fit_transform(self.Vocab.data)
-			sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
-			X_new=sel.fit_transform(X)
+			self.vectorizer = VarianceThreshold(threshold=(.8 * (1 - .8)))
+			X_new=self.vectorizer.fit_transform(X)
 		else:
 			print 'Please Select a defined type of feature'
-		return X_new, self.Vocab.target
+			
+		X_test=self.vectorizer.transform(self.Test_cab.data)
+                y_test=self.Test_cab.target
+		return X_new,self.Vocab.target,X_test,y_test
+		
 	def word2vecmodel(self):
 		b=gensim.models.Word2Vec(brown.sents())
                 b.save('/home/sonali/Desktop/corporamodel')
